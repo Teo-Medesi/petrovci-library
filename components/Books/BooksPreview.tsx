0e1c8f88b16b835bd9d47b1e10cf24671c3335d2
Teo-Medesi <teo.medesi@gmail.com>
2024-05-06 19:48:44 +0200

    no cover image design looks better

--- components/Books/BooksPreview.tsx
+++ components/Books/BooksPreview.tsx
@@ -5,13 +5,19 @@ import React from 'react'
 const Book = ({ book }: { book: Book }) => {
   return (
     <div className='cursor-pointer w-full lg:w-[128px] lg:min-h-[194px] flex flex-col items gap-2'>
-      {
-        book?.cover_image 
-          ?
-          <img className='rounded w-[128px] h-[194px]' src={book.cover_image} alt="cover image of book" />
-          :
-          <div className='rounded w-[128px] h-[194px] border border-gray-500 flex justify-center items-center'>no cover image</div>
-      }
+{
+  book?.cover_image 
+    ?
+    <img className='rounded w-[128px] h-[194px]' src={book.cover_image} alt="cover image of book" />
+    :
+    <div className='rounded w-[128px] h-[194px] border border-gray-500 flex justify-center items-center bg-gradient-to-br from-blue-500 to-indigo-700 text-white'>
+      <div className="text-center">
+        <h3 className="text-lg font-semibold">{book.book_title}</h3>
+        <p className="text-sm">{book.author_full_name}</p>
+      </div>
+    </div>
+}
+
       <p className='text-gray-500'>{book.author_full_name}</p>
       <p className='text-lg'>{book.book_title}</p>
       {book.available ? <p className='mt-4 text-green-500'>Dostupno</p> : <p className='mt-4 text-red-500'>Nedostupno</p>}
